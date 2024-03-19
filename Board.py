b46aba65f473ba3e3231ba837d8bfe8539a71840
Bartosz Warchoł <bartex132@gmail.com>
2024-03-19 22:03:39 +0100

    Cleaned code

--- Board.py
+++ Board.py
@@ -6,18 +6,5 @@ class Board:
         # Create "2D" list
         # self.board = [[None for y in range(height)] for x in range(width)]
         
-    def fill_boundaries(self, screen):
+    def draw_boundaries(self, screen):
         screen.border('|', '|', '-', '-', '+', '+', '+', '+') 
-        # screen.addstr(0, 0, '+')
-        # for _ in range(0, self.width - 2):
-        #     screen.addstr('-')
-        # screen.addstr('+')
-
-        # for i in range(0, self.height - 2):
-        #     screen.addstr(i + 1, 0, '|')
-        #     screen.addstr(i + 1, self.width - 1, '|')
-
-        # screen.addstr(self.height - 1, 0, '+')
-        # for _ in range(0, self.width - 2):
-        #     screen.addstr('-')
-        # screen.addstr('+')
